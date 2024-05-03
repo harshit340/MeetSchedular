b1493496acf70283d4ef2e1d4909d83385a03a60
harshit <harshitshrivastava340@gmail.com>
2024-05-03 23:44:44 +0530

    done with calender

--- meetschedular/src/app/calender/page.tsx
+++ meetschedular/src/app/calender/page.tsx
@@ -27,17 +27,18 @@ export default function calender() {
         <>
          <Card size="small" style={{ height: '600px' , margin:"auto" }} className="basic-layout">
            <div className="main-content">
-            <div>
-                <SlArrowLeftCircle style={{fontSize:"40px" , color:"blue" , cursor:"pointer" }}/> 
+            <SlArrowLeftCircle style={{fontSize:"40px" , color:"blue" , cursor:"pointer" }}/> 
+            <div className='content-part'>
+               
                 <p style={{fontSize:"15px" , fontWeight:300}}>Harshit Shrivastava</p>
-                <p style={{fontSize:"28px" , fontWeight:300}}>30 Minute Meeting</p>
+                <p style={{fontSize:"28px" , fontWeight:500 }}>30 Minute Meeting</p>
                 <p style={{fontSize:"12px" , fontWeight:300 }}><FaRegClock style={{fontSize:"12px" , fontWeight:300}}/>  30 min</p>
                 <p style={{fontSize:"14px" , fontWeight:300}}><BsCameraVideo />  Web conferencing details provide upon confirmation</p>
                 </div>
-            <div>
-            <h2>Select date and time</h2>
+            <div className='calender-part'>
+            <h2 style={{padding:"20px"}}>Select date and time</h2>
             <div style={wrapperStyle}><Calendar fullscreen={false} onPanelChange={onPanelChange} /></div>
-            <div>
+            <div style={{padding:"20px"}}>
                 <h3>Time Zone</h3>
                 <p>Indian Standard Time (4:55)</p>
             </div>
